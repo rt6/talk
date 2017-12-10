17d598f0a8dda94d41bd04972ca1e22caecbea40
rt6 <rt6@users.noreply.github.com>
2017-12-10 16:35:52 +1100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='talk',
+      version='0.1.0',
+      description='talk module',
+      url='https://github.com/rt6/talk',
+      author='autho name',
+      author_email='author email',
+      license='',
+      packages=['talk'],
+zip_safe=False)
